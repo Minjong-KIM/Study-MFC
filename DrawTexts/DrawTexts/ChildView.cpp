fc64a5584a78959d13f30178a48f57018d9bd32b
Minjong-KIM <dure7108@naver.com>
2021-12-07 18:15:30 +0900

    first commit

---
+++ DrawTexts/DrawTexts/ChildView.cpp
@@ -0,0 +1,64 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "DrawTexts.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this);
+
+	CRect rect;
+	GetClientRect(&rect);
+
+	dc.SetTextColor(RGB(255, 0, 0));
+	dc.SetBkColor(RGB(255, 255, 0));
+	dc.DrawText(_T("DrawText [1]"), &rect, 0);
+	dc.DrawText(_T("DrawText [2]"), &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+	dc.SetTextAlign(TA_CENTER);
+	dc.SetTextColor(RGB(0, 0, 255));
+	dc.SetBkColor(RGB(0, 255, 0));
+	dc.TextOut(rect.right / 2, rect.bottom / 4 * 3, _T("TextOut"));
+}
+
---
+++ DrawTexts/DrawTexts/DrawTexts.md
@@ -0,0 +1,6 @@
+# DrawTexts
+1. TextOut() ->(x, y)에 문자열을 출력한다.
+2. DrawText() -> 직사각형 영역 내부에 문자열을 출력한다.
+3. SetTextColor() -> 글자의 색 지정.
+4. SetBkColor() -> 글자의 배경색 지정.
+5. SetTextAlign() -> 기준 위치에 대한 문자열 정렬 방식 설정.
