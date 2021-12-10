3d368345dd6efabec15603a31eda79a7f000e2c6
Minjong-KIM <dure7108@naver.com>
2021-12-10 13:35:04 +0900

    first commit

---
+++ ChangeCursor/ChangeCursor/ChangeCursor.md
@@ -0,0 +1,3 @@
+# Chage Cursor
+1. ::GetCursorPos() 함수로 커서의 스크린 좌표를 얻을 수 있다.
+2. ::SetCursor(AfxGetApp() -> LoadCursor()) 커서를 바꿀 수 있다.
---
+++ ChangeCursor/ChangeCursor/ChildView.cpp
@@ -0,0 +1,79 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ChangeCursor.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_SETCURSOR()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this);
+
+	dc.SelectStockObject(LTGRAY_BRUSH);
+	dc.Ellipse(10, 10, 400, 100);
+}
+
+
+
+BOOL CChildView::OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message)
+{
+	if (nHitTest == HTCLIENT) {
+		CPoint point;
+		::GetCursorPos(&point);
+		ScreenToClient(&point);
+
+		CRgn rgn;
+		rgn.CreateEllipticRgn(10, 10, 400, 100);
+
+		if (rgn.PtInRegion(point)) {
+			::SetCursor(AfxGetApp()->LoadCursor(IDC_CURSOR1));
+		}
+		else {
+			::SetCursor(AfxGetApp()->LoadStandardCursor(IDC_CROSS));
+		}
+
+		return TRUE;
+	}
+
+	return CWnd::OnSetCursor(pWnd, nHitTest, message);
+}
