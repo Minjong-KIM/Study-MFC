21f8ba586618aeba522c19730829e1b100b94ce2
Minjong-KIM <dure7108@naver.com>
2021-12-16 13:22:05 +0900

    first commit

---
+++ ModalDialog/ModalDialog/CMyDialog.cpp
@@ -0,0 +1,65 @@
+﻿// CMyDialog.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ModalDialog.h"
+#include "CMyDialog.h"
+#include "afxdialogex.h"
+
+
+// CMyDialog 대화 상자
+
+IMPLEMENT_DYNAMIC(CMyDialog, CDialog)
+
+CMyDialog::CMyDialog(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_CMyDialog, pParent)
+{
+
+}
+
+CMyDialog::~CMyDialog()
+{
+}
+
+void CMyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_FONT, m_combo);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDialog, CDialog)
+	ON_BN_CLICKED(IDC_CLEAR, &CMyDialog::OnBnClickedClear)
+END_MESSAGE_MAP()
+
+
+// CMyDialog 메시지 처리기
+
+
+void CMyDialog::OnBnClickedClear()
+{
+	EndDialog(IDC_CLEAR);
+}
+
+
+BOOL CMyDialog::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	SetDlgItemText(IDC_STR, m_str);
+	m_combo.SetCurSel(m_font);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void CMyDialog::OnOK()
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	GetDlgItemText(IDC_STR, m_str);
+	m_font = m_combo.GetCurSel();
+
+	CDialog::OnOK();
+}
---
+++ ModalDialog/ModalDialog/ModalDialog.md
@@ -0,0 +1,6 @@
+# Dialog
+1. Modal -> 대화 상자를 닫아야 응용 프로그램이 다른 작업을 할 수 있다.
+2. Modeless -> 대화 상자를 닫지 않아도 응용 프로그램이 다른 작업을 할 수 있다.
+# Modal
+1. CDialog 객체를 만든다.
+2. CDialog::DoModal() 함수를 호출한다.
