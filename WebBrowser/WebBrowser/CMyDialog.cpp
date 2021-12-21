2746996188d4630103fc25cdba84d398e54aee13
Minjong-KIM <dure7108@naver.com>
2021-12-21 13:41:49 +0900

    first commit

---
+++ WebBrowser/WebBrowser/CMyDialog.cpp
@@ -0,0 +1,36 @@
+﻿// CMyDialog.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "WebBrowser.h"
+#include "CMyDialog.h"
+#include "afxdialogex.h"
+
+
+// CMyDialog 대화 상자
+
+IMPLEMENT_DYNAMIC(CMyDialog, CDialogEx)
+
+CMyDialog::CMyDialog(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+	, m_url(_T(""))
+{
+
+}
+
+CMyDialog::~CMyDialog()
+{
+}
+
+void CMyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_url);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDialog, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMyDialog 메시지 처리기
